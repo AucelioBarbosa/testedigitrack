9b24f862043d8c3b8605a5f29c09c38ef5dab376
Aucélio Barbosa <aucelio.barbosa@gmail.com>
2021-03-27 19:26:24 -0300

    cross erro correction

--- digitrack/src/main/java/com/example/demo/resources/PesonResources.java
+++ digitrack/src/main/java/com/example/demo/resources/PesonResources.java
@@ -3,6 +3,7 @@ package com.example.demo.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/v1")
 public class PesonResources {
